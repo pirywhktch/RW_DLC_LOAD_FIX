6942fe9e42203d8f907f3542058bae7860d618e9
pirywhktch <edicomna@gmail.com>
2026-06-22 17:18:18 +0300

    Initial commit

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿
+
+using BepInEx;
+using Menu;
+using UnityEngine;
+using Menu.Remix.MixedUI;
+
+[BepInPlugin("DLCFIX", "DLC loader fix", "1.0")]
+public class DLCFIX : BaseUnityPlugin
+{
+    public void OnEnable()
+    {
+        // --- DLC FIX ---
+        Logger.LogInfo("DLCFIX: loaded");
+        On.RainWorld.Start += (orig, self) =>
+        {
+            orig(self);
+
+            if (!self.dlcsInstalled.Contains("downpour"))
+            {
+                self.dlcsInstalled.Add("downpour");
+                Logger.LogInfo("DLCFIX: added downpour back to remix");
+            }
+            if (!self.dlcsInstalled.Contains("watcher"))
+            {
+                self.dlcsInstalled.Add("watcher");
+                Logger.LogInfo("DLCFIX: added watcher back to remix");
+            }
+
+
+            Logger.LogInfo("DLCFIX: reset and enabled detection of installed DLCs");
+        };
+
+
+    }
+
+}
